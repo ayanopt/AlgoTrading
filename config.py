7a413148b42f43fb6a1a0ef0199e6ec28b250890
ayanopt <93662645+ayanopt@users.noreply.github.com>
2022-03-24 17:55:23 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+api_key='TDA_api_key' #this is essential for api verification
+token_path= "file_path/chalicelib/token"
+redirect_url="https://localhost" 
+#local host is used to sign back into TD API to verify trades made with this app
